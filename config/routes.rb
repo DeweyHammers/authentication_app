26f5ed03f71d18ed160815913372a4f2f95a1b24
dewey <hammersdrichard@gmail.com>
2021-09-11 10:19:13 -0700

    app done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resource :sessions, only: [:create]
+  resource :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_id, to: "seeions#logged_in"
   root to: "static#home"
 end
