d7b2b231e4147e5f2d97365de8fdd841c4d199be
dewey <hammersdrichard@gmail.com>
2021-09-12 22:45:43 -0700

    changed save user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < ApplicationController
   def create
-    user = User.create(email: params['user']['email'], 
+    user = User.new(email: params['user']['email'], 
       password: params['user']['password'], 
       password_confirmation: params['user']['password_confirmation'])
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
